565b07aa31eb62291a724748f87a4f8288232027
Nilay Patel <nilayp@gmail.com>
2025-09-26 15:40:10 -0700

    Added mecanum wheel classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCentricMecanumTeleOp.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class RobotCentricMecanumTeleOp extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double forward = gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double turn = -gamepad1.right_stick_x;
+            double strafe = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+
+            double maxDrivePower = 0.3;
+
+            forward = forward * maxDrivePower;
+            turn = turn * maxDrivePower;
+            strafe = strafe * maxDrivePower;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(forward) + Math.abs(strafe) + Math.abs(turn), 1);
+            double frontLeftPower = (forward + strafe + turn) / denominator;
+            double frontRightPower = (forward - strafe - turn) / denominator;
+            double backLeftPower = (forward - strafe + turn) / denominator;
+            double backRightPower = (forward + strafe - turn) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backLeftMotor.setPower(backLeftPower);
+            backRightMotor.setPower(backRightPower);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VerySimpleMecanumTeleOp.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class VerySimpleMecanumTeleOp extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double forward = gamepad1.left_stick_y;
+            double turn = -gamepad1.right_stick_x;
+            double strafe = -gamepad1.left_stick_x;
+
+            double maxDrivePower = 0.3;
+
+            forward = forward * maxDrivePower;
+            turn = turn * maxDrivePower;
+            strafe = strafe * maxDrivePower;
+
+            double frontLeftPower = (forward + turn + strafe);
+            double frontRightPower = (forward - turn - strafe);
+            double backLeftPower = (forward + turn - strafe);
+            double backRightPower = (forward - turn + strafe);
+
+            frontLeftMotor.setPower(frontLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backLeftMotor.setPower(backLeftPower);
+            backRightMotor.setPower(backRightPower);
+
+            telemetry.addData("frontLeftDrive", frontLeftPower);
+            telemetry.addData("frontRightDrive", frontRightPower);
+            telemetry.addData("backLeftDrive", backLeftPower);
+            telemetry.addData("backRightDrive", backRightPower);
+            telemetry.addData("forward", forward);
+            telemetry.addData("turn", turn);
+            telemetry.addData("Strafe", strafe);
+            telemetry.update();
+        }
+    }
+}
